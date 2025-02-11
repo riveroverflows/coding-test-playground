5e043142bdbf89863098bff24d23de80cf327e39
river <river@antigravity.kr>
2025-02-12 03:26:22 +0900

    boj 10870 피보나치수 5 - 반복문 + 메모이제이션 풀이

---
+++ boj/10870_피보나치수5/for_and_memoization.py
@@ -0,0 +1,15 @@
+# 반복문 + 메모이제이션
+
+# 초기화
+num = int(input())
+array = [-1] * (num + 2)
+
+# base case
+array[0] = 0
+array[1] = 1
+
+# recursive
+for i in range(2, num+1):
+	array[i] = array[i-2] + array[i-1] 
+
+print(array[i])
