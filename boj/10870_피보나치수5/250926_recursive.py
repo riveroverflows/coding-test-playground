836bbd53c4c32029bc96023629008b47cab3d736
river <bluuminn@gmail.com>
2025-09-27 00:33:38 +0900

    boj: #10870 피보나치수5 재귀로 풀이

---
+++ boj/10870_피보나치수5/250926_recursive.py
@@ -0,0 +1,19 @@
+num = int(input())
+memo = [-1] * (num+1)
+
+def fibo(n):
+	if memo[n] != -1:
+		return memo[n]
+
+	# base case
+	if n == 0:
+		return 0
+	if n == 1:
+		return 1
+
+	# recursive case
+	memo[n] = fibo(n-1) + fibo(n-2)
+	return memo[n]
+
+
+print(fibo(num))
